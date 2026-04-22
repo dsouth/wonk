a3410482a1e1974609ad6969697b6dec809b3124
Doug South <doug.south@gmail.com>
2026-04-22 21:13:45 +1000

    ... and so it begins ...

---
+++ include/extern.hpp
@@ -0,0 +1,11 @@
+extern "C" {
+#define WLR_USE_UNSTABLE
+#include <wlr/util/log.h>
+
+#define static
+
+#include <wlr/backend.h>
+#include <wlr/backend/session.h>
+
+#undef static
+}
---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject wonk "0.1-SNAPSHOT"
+  :license {:name "MIT License"
+            :url "https://mit-license.org/"}
+  :dependencies []
+  :plugins [[org.jank-lang/lein-jank "0.6"]]
+  :middleware [leiningen.jank/middleware]
+  :main wonk.main
+  :jank {:include-dirs ["include" 
+                        "/usr/include/wlroots-0.20/"
+                        ;; not sure about this one...
+                        ;; not required for C ???
+                        "/usr/include/pixman-1/"
+                        ]
+         :library-dirs ["/usr/lib/"]
+         :linked-libraries ["wlroots-0.20"]}
+
+  :profiles {:base {:jank {:output-dir "target/debug"
+                           :optimization-level 0}}
+             :release {:jank {:output-dir "target/release"
+                              :optimization-level 2}}})
