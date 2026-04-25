548b686811713a89ac631a76497d1af4a45fc1bc
Doug South <doug.south@gmail.com>
2026-04-25 23:10:20 +1000

    progress, though wlr_log function isn't being found... :/

--- include/extern.hpp
+++ include/extern.hpp
@@ -5,8 +5,18 @@ extern "C" {
 #define static
 
 #include <wlr/backend.h>
-#include <wlr/backend/session.h>
+#include <wlr/render/allocator.h>
 #include <wlr/render/wlr_renderer.h>
+#include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_cursor.h>
+#include <wlr/types/wlr_data_device.h>
+#include <wlr/types/wlr_output_layout.h>
+#include <wlr/types/wlr_scene.h>
+#include <wlr/types/wlr_seat.h>
+#include <wlr/types/wlr_subcompositor.h>
+#include <wlr/types/wlr_xcursor_manager.h>
+#include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/util/log.h>
 
 #undef static
 }
